7f5535cf05a77a122e616b97be7b83842840f68a
Dominic Yeboah <yeboahd24@gmail.com>
2023-03-23 11:54:34 +0000

    kolo debugging externsion added

--- Sortable/settings.py
+++ Sortable/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "kolo.middleware.KoloMiddleware",
 ]
 
 ROOT_URLCONF = 'Sortable.urls'
